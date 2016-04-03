ac783e55f6523db9dec4a0baab0eb4b70d637301
Ozan Deniz <denzozan@gmail.com>
2016-04-03 21:00:50 +0300

    Add leader board app

--- DjangoPlayground/urls.py
+++ DjangoPlayground/urls.py
@@ -19,4 +19,5 @@ from django.contrib import admin
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^poll/', include('poll.urls')),
+    url(r'', include('leaderboard.urls')),
 ]
